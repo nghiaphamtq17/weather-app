6f3c590e353fc5af743b558bfc2e45dee2ff6971
NghiaPhamtq17 <88359467+nghiaphamtq17@users.noreply.github.com>
2021-10-01 06:49:06 +0700

    update to develop

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -1,33 +1,28 @@
 import React from "react";
+import { calTemp } from "./helper";
 
 const Share = (props) => {
   const { data } = props;
 
-  const calTemp = (value) => {
-    return value / 10;
-  };
-
   return (
     <div className="container text-center">
-      <h1>Tỉnh / Thành Phố : {data?.city?.name}</h1>
-      <p>Thời tiết : {(() => {
-                switch (
-                  data?.list[0]?.weather[0]?.main
-                ) {
-                  case "Clouds":
-                    console.log("clouds");
-
-                    return "Gió vcl";
-                  case "Rain":
-                    console.log("rain");
-                    return "mưa vcl "
-                    
-                  default:
-                    return "Đoán xem";
-                }
-              })()}</p>
-      <p>Nhiệt độ : {calTemp(data?.list[0].main.temp) || 'Chưa cập nhật'} °C</p>
-      <p>Tốc độ gió : {data?.list[0]?.clouds.all || "Chưa cập nhật"}</p>
+      <h1>Tỉnh / Thành Phố : {data?.city?.name || ""}</h1>
+      <p>
+        Thời tiết :{" "}
+        {(() => {
+          // TODO: tach function
+          switch (data?.list[0]?.weather[0]?.main) {
+            case "Clouds":
+              return "Gió vcl";
+            case "Rain":
+              return "mưa vcl ";
+            default:
+              return "Đoán xem";
+          }
+        })()}
+      </p>
+      <p>Nhiệt độ : {calTemp(data?.list[0].main?.temp) || "Chưa cập nhật"} °C</p>
+      <p>Tốc độ gió : {data?.list[0]?.clouds?.all || "Chưa cập nhật"}</p>
     </div>
   );
 };
